4639f87743e0228f5a1147b7bfb148da855491e1
aadii-19 <adithpramod197@gmail.com>
2025-10-07 15:55:06 +0530

    Update task by ID

--- src/main/java/com/Adith/taskManager/controller/TaskController.java
+++ src/main/java/com/Adith/taskManager/controller/TaskController.java
@@ -20,7 +20,7 @@ public class TaskController {
 
     //  -------------------------- REGISTER TASK --------------------------------
     //  -------------------------------------------------------------------------
-    @PostMapping("/")
+    @PostMapping("/tasks")
     public ResponseEntity<?> addTask(@PathVariable long userId, @PathVariable long projectId, @RequestBody Task task){
         try{
             taskService.addTask(userId,projectId,task);
@@ -30,6 +30,22 @@ public class TaskController {
         } catch (Exception e) {
             Map<String, String> body = new HashMap<>();
             body.put("message","task adding unsuccessful");
+            return new ResponseEntity<>(body, HttpStatus.OK);
+        }
+    }
+
+    //  --------------------- UPDATE TASK BY TASK_ID ----------------------------
+    //  -------------------------------------------------------------------------
+    @PutMapping("/tasks/{taskId}")
+    public ResponseEntity<?> updateTask(@PathVariable long userId, @PathVariable long projectId,@PathVariable long taskId, @RequestBody Task task){
+        try{
+            taskService.updateTask(userId,projectId,taskId,task);
+            Map<String, String> body = new HashMap<>();
+            body.put("message","task updated successfully! ");
+            return new ResponseEntity<>(body, HttpStatus.OK);
+        } catch (Exception e) {
+            Map<String, String> body = new HashMap<>();
+            body.put("message","task update unsuccessfully! ");
             return new ResponseEntity<>(body, HttpStatus.OK);
         }
     }
--- src/main/java/com/Adith/taskManager/service/TaskService.java
+++ src/main/java/com/Adith/taskManager/service/TaskService.java
@@ -19,6 +19,8 @@ public class TaskService {
         this.userService = userService;
     }
 
+    //  --------------------- ADDING TASK BY PROJECT_ID -------------------------
+    //  -------------------------------------------------------------------------
     public void addTask(long userId, long projectId, Task task) {
         Project proj = projectService.getProjectById(userId,projectId);
         User owner = userService.getUserById(userId);
@@ -35,5 +37,29 @@ public class TaskService {
                 throw new RuntimeException("Owner doesn't exist");
             }
         }
+    }
+
+    //  --------------------- UPDATE TASK BY TASK_ID ----------------------------
+    //  -------------------------------------------------------------------------
+    public void updateTask(long userId, long projectId, long taskId, Task task) {
+        /*
+        1. Get the prev task using taskId
+        2. Check if the projId is authentic
+        3. Check the authenticity of the userId
+        4. If all true, then update old with new task
+         */
+        Task prevTask = taskRepo.findById(taskId).
+                orElseThrow(() -> new RuntimeException("Task Not Found"));
+        if(prevTask.getProject().getId()!=projectId){
+            throw new RuntimeException("Task doesn't belong to this project");
+        }
+        if(prevTask.getProject().getOwner().getId()!=userId){
+            throw new RuntimeException("You don't have permission to modify this!");
+        }
+        prevTask.setTitle(task.getTitle());
+        prevTask.setDescription(task.getDescription());
+        prevTask.setPriority(task.getPriority());
+        prevTask.setStatus(task.getStatus());
+        prevTask.setDeadLine(task.getDeadLine());
     }
 }
